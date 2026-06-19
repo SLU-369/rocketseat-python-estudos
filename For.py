a13d77906f43b5c49d6df6e7ea552a11cd591827
Samuel Uchoa <samuel.uchoadev@gmail.com>
2026-06-18 23:50:24 -0300

    Study: Range, len e enumerate em for

--- For.py
+++ For.py
@@ -34,3 +34,30 @@ print("\n For utilizando dicionario - Chaves e valores")
 for chaves, valores in pessoa.items():
     print("\n")
     print(f"{chaves}: {valores}")
+
+# range (): Retorna um intervalo de numero em formato de lista. Ex:
+
+numeros = list(range(10))
+print("\n", numeros)
+
+#len (): pregunt: Quantos elementos existem aqui?
+
+print("\n Utilizando a função renge() com len()")
+lista = list(range(6))
+print("\n", lista)
+for indice in range(0, len(lista)):#6 elementos mas como o range diminui 1 vai ficar 4 o indice  
+    print("indice:", indice)
+    print('elemento: ', lista[indice])
+if indice == 3:
+    lista[indice] = 5
+else:
+    lista[indice] = 0
+print(lista)
+
+#Enumerate () : me retorna o indice e o valor junto sem eu precisar Lista[indice]. 
+
+lista_enumerate = ["a", "B", "C"]
+for indice, valor in enumerate(lista_enumerate):
+    print("\n", f"{indice}: {valor}")
+    if indice == 1:
+        print("indice 1")
